5c846a8010dc74461455d093bbe41c95c3ca2d94
ur1el0 <zanoroosc@gmail.com>
2026-09-10 17:31:09 +0800

    feat: apply SwiftUI app files and assets to Sources

---
+++ Sources/FableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fable_iosApp.swift
+//  Fable-ios
+//
+//  Created by Mac-LAB on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
